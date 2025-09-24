a2bef36be0b9629ffadf73a9d7efa64528864a97
Taylor Poe <t_poe25@yahoo.com>
2025-09-24 15:02:56 -0500

    pag

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -4,12 +4,14 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class for paginating and managing a database of popular baby names.
+    Class gives functionality to load, cache, paginate data from a CSV file
+    containing popular baby names. Includes deletion-resilient pagination.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,7 +20,8 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,7 +32,8 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """
+        Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -39,5 +43,29 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """Get a page of data with deletion-resilient pagination."""
+        assert 0 <= index < len(self.indexed_dataset())
+
+        data = []
+        next_index = index
+        dataset = self.indexed_dataset()
+
+        # Gather data for the current page
+        while len(data) < page_size and next_index < len(dataset):
+            if next_index not in dataset:
+                next_index += 1
+                continue
+            data.append(dataset[next_index])
+            next_index += 1
+
+        # Determine the next index for the following page
+        while next_index < len(dataset) and next_index not in dataset:
+            next_index += 1
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': page_size,
+            'data': data
+        }
