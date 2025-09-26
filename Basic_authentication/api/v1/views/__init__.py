bffe8df9407f5075fba247de283ca01765da370b
Taylor Poe <t_poe25@yahoo.com>
2025-09-26 12:27:06 -0500

    fix link

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -1,2 +1,7 @@
-#!/usr/bin/env python3
-from .index import app_views
+from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Import routes so they get registered
+from api.v1.views import index
+from api.v1.views import users
