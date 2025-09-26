a230bccbfaede1c16cb47eb88f032ab18a22f03e
Taylor Poe <t_poe25@yahoo.com>
2025-09-26 12:31:49 -0500

    trying to seee error

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -13,3 +13,10 @@ def status():
 def unauthorized():
     """Endpoint that aborts with 401"""
     abort(401)
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app_views.route('/unauthorized', methods=['GET'])
+def forbidden():
+    """Endpoint that aborts with 403"""
+    abort(403)
+    return jsonify({"error": "Forbidden"}), 403
