6d1d1dfb644fa7a35864ac5ef272a7f8b29e1353
Taylor Poe <t_poe25@yahoo.com>
2025-09-27 00:33:44 -0500

    doc

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import routes so they get registered
+"""API authentication module.
+"""
 from api.v1.views import index
 from api.v1.views import users
+from api.v1 import auth
