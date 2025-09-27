8755c393b9635f0e490d659695c1cf3aaefd3156
Taylor Poe <t_poe25@yahoo.com>
2025-09-27 15:13:18 -0500

    checking doc 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 """Route module for the API"""
 from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
+from api.v1.views import app_views
 
-
+# Create the Flask application
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+# Init auth to None
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+# Determine the authentication type based on environment variable
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    """Before request handler to manage authentication"""
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+# Error handlers for common HTTP errors
 @app.errorhandler(401)
 def unauthorized(error: HTTPException):
     """
