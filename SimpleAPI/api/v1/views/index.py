9f52ba5fb5bf125f926030c8d22a61a61ef2b4aa
Taylor Poe <t_poe25@yahoo.com>
2025-09-26 23:59:57 -0500

    fix doc

--- SimpleAPI/api/v1/views/index.py
+++ SimpleAPI/api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
+def status():
     """ GET /api/v1/status
     Return:
       - the status of the API
@@ -15,7 +15,7 @@ def status() -> str:
 
 
 @app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
+def stats():
     """ GET /api/v1/stats
     Return:
       - the number of each objects
