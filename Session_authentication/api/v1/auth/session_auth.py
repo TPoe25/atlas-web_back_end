7a7b2613a7b2b61b473f1aec59ce0c2db36fc3bd
Taylor Poe <t_poe25@yahoo.com>
2025-09-27 20:34:48 -0500

    checking

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -33,3 +33,18 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieve the user ID associated with a given session ID
+        Args:
+            session_id (str): The session ID for which to retrieve the user ID
+        Returns:
+            str: The user ID associated with the session ID or None if not found
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
