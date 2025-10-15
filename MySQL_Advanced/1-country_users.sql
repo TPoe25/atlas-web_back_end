2fb3ab1c1f49af9784ff3ced126891d8283dfc20
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 13:20:44 -0500

    finished 1 after some troubles... but i got it bad syntax

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- SQL script to create table users
+-- if the table already exists scipt should work
+-- Drop TABLE IF EXISTS users;
+Drop TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN')
+);
