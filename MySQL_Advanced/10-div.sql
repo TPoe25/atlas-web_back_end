421533cd1a1233cc2931485d9ca3a1a9530cd2c3
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 16:49:36 -0500

    finish 10

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,21 @@
+-- 10-div.sql
+-- Function that divides 2 numbers safely
+
+DELIMITER $$
+
+CREATE FUNCTION SavDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT;
+
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+
+    RETURN result;
+END $$
+
+DELIMITER ;
