da7697b42608489a57116db4a827a33a52965fa1
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 13:42:03 -0500

    finally got output

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,11 +1,10 @@
--- SQL script to create table users
--- if the table already exists scipt should work
--- Drop TABLE IF EXISTS users;
-Drop TABLE IF EXISTS users;
+-- Drop the table is exists
+DROP TABLE IF EXISTS users;
 
+-- creates the users table
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
