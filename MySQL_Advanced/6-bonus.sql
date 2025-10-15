961e121dcc1d8e8e65e9eb7e4634cbb56315b004
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 15:54:49 -0500

    fix

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,33 +1,44 @@
--- AddBonus where it takes 3 imputs
-
-DELIMITER $$
+DELIMITER $
+$
 
 CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
+    IN p_user_id INT,
+    IN p_project_name VARCHAR
+(255),
+    IN p_score INT
 )
 BEGIN
-    DECLARE project_id INT;
+    DECLARE v_project_id INT DEFAULT 0;
+DECLARE
+CONTINUE
+HANDLER FOR NOT FOUND
+SET v_project_id
+= NULL;
 
-    -- init to NULL
-    SET project_id = NULL;
+-- Try to get the project_id
+SELECT id
+INTO v_project_id
+FROM projects
+WHERE name = p_project_name
+LIMIT 1;
 
-    -- Get project id from user_id and project_name
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name;
-    LIMIT 1;
+-- If project does not exist, insert it
+IF v_project_id IS NULL THEN
+INSERT INTO projects
+    (name)
+VALUES
+    (p_project_name);
+SET v_project_id
+= LAST_INSERT_ID
+();
+END
+IF;
 
-    -- if not found, insert the project
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
-    END IF;
-
-    -- Insert new correction
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score);
+    -- Insert the new correction
+    INSERT INTO corrections
+    (user_id, project_id, score)
+VALUES
+    (p_user_id, v_project_id, p_score);
 END$$
 
 DELIMITER ;
