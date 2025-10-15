20610d2b5ca867ee7475b05e04f5cb186a9b6def
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 14:42:21 -0500

    had equal instad of like

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -5,5 +5,5 @@ SELECT
     band_name,
     COALESCE(split, YEAR(CURDATE())) - formed AS lifespan
 FROM metal_bands
-WHERE style = '%Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
