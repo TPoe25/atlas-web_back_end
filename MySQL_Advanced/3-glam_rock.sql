9c43a1e613f604362ef66897edf47c5150784957
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 14:59:15 -0500

    fixed case

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,10 @@
 
 SELECT
     band_name,
-    COALESCE(`split`, 2020) - formed AS lifespan
+    CASE
+        WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+        ELSE split - formed
+    END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
