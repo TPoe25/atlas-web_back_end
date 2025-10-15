d74fc6977344522954cebe86921df1df28758bb0
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 14:51:30 -0500

    fixed split

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,9 @@
 -- Script that runs teh origins of bands base on total numbers
-
 -- Select the band name and origin country
+
 SELECT
     band_name,
-    COALESCE(split, 2020) - formed AS lifespan
+    COALESCE(`split`, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
