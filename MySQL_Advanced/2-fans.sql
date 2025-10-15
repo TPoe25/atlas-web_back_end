12b40bfb9245054694643ae7e7ee92862803650c
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 14:25:57 -0500

    fix 2

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,10 @@
+-- Script that runs teh origins of bands base on total numbers
+
+-- Select the band name and origin country
+SELECT
+    origin,
+    SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+-- Order by the total number of fans in descending order
+ORDER BY nb_fans DESC;
