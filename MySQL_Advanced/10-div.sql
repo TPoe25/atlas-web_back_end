6a5db8342c1d1b90cd5e2d15055e28c484f6957c
Taylor Poe <t_poe25@yahoo.com>
2025-10-15 17:03:54 -0500

    checking

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -7,15 +7,10 @@ CREATE FUNCTION SavDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-    DECLARE result FLOAT;
-
     IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
+        RETURN 0;
     END IF;
-
-    RETURN result;
+    RETURN a / b;
 END $$
 
 DELIMITER ;
