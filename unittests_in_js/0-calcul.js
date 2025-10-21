1d4a9b5f634c9f0e3207c637ef17e9e91a679091
Taylor Poe <t_poe25@yahoo.com>
2025-10-21 15:14:29 -0500

    npm test not working but got it with mpn exec mocha ./0-calcul.test.js

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,8 @@
+// 0-calcul.js
+// Func that rounds a and b, then returns their sum
+
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,22 @@
+// 0-calcul.test.js
+// using Node's built-in 'assert' module for testing
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+    it('should return 4 when given (1, 3', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should round second number up and return 5 for (1, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+   it('should round second number up and return 5 for (1.2, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should round both up and return 6 for (1.5, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
