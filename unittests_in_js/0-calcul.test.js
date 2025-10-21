d73c65e1ed52e4903b6cafb724ad7e199fdd92cf
Taylor Poe <t_poe25@yahoo.com>
2025-10-21 15:22:22 -0500

    forgot )

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('should return 4 when given (1, 3', () => {
+    it('should return 4 when given (1, 3)', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
 
