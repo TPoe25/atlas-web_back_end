6bd3a11f15eb929f5ca6d877b2b5a946d8804d43
Taylor Poe <t_poe25@yahoo.com>
2025-10-29 14:43:55 -0500

    fixed

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,21 +1,22 @@
 // 3-payment.test.js
 
-const { expect } = require('chai');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require("sinon");
+const { expect } = require("chai");
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
 
-describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    //spy on Ut
-    const consoleSpy = sinon.spy(console, 'log');
+describe("sendPaymentRequestToApi", function () {
+  it("should call Utils.calculateNumber with correct arguments", function () {
+    // Spy on Utils.calculateNumber
+    const spy = sinon.spy(Utils, "calculateNumber");
 
+    // Call the function being tested
     sendPaymentRequestToApi(100, 20);
 
-    // Check that Utils.calculateNumber was called once with the correct arguments
-    expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    // Check that the spy was called with the correct args
+    expect(spy.calledOnceWithExactly("SUM", 100, 20)).to.be.true;
 
-    // Restore console.log for other tests
-    consoleSpy.restore();
+    // Restore the spy
+    spy.restore();
   });
 });
