62aab21bbaac26689f45ee72bfbda55797326a6f
Taylor Poe <t_poe25@yahoo.com>
2025-10-29 15:11:16 -0500

    check

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,23 +1,28 @@
 // 3-payment.test.js
 
 const sinon = require("sinon");
+const { expect } = require("chai");
 const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
 
-describe('sendPaymentRequestToApi', function () {
-  it('logs the correct message', function () {
-    // Spy on Utils.calculateNumber
-    const logSpy = sinon.spy(console, 'log');
+describe("sendPaymentRequestToApi", function () {
+  it("should call Utils.calculateNumber with correct arguments", function () {
+    const spy = sinon.spy(Utils, "calculateNumber");
 
-    // Call the function being tested
     sendPaymentRequestToApi(100, 20);
 
-    // Check that the spy was called with the correct args
-    if (!logSpy.calledWithExactly('The total is: 120')) {
-        console.log('Expected console log did not match');
-    }
+    expect(spy.calledOnceWithExactly("SUM", 100, 20)).to.be.true;
 
-    // Restore the spy
+    spy.restore();
+  });
+
+  it("logs the correct message", function () {
+    const logSpy = sinon.spy(console, "log");
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(logSpy.calledOnceWithExactly("The total is: 120")).to.be.true;
+
     logSpy.restore();
   });
 });
